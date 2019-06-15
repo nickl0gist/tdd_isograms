3db15480cce6bde6ec6c2817796a62a39346e60e
nickl0gist <nicklogistick@mail.ru>
2019-06-15 13:42:01 +0200

    Isogram

--- src/main/java/IsogramChecker.java
+++ src/main/java/IsogramChecker.java
@@ -1,1 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
 
+public class IsogramChecker {
+
+    public boolean isIsogram(String input) {
+        input = input.replace(" ", "").replace("-","");
+        input = input.toLowerCase();
+
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            set.add(input.charAt(i));
+        }
+        return set.size() == input.length();
+    }
+}
--- src/test/java/IsogramCheckerTest.java
+++ src/test/java/IsogramCheckerTest.java
@@ -11,64 +11,48 @@ public class IsogramCheckerTest {
         assertTrue(iso.isIsogram(""));
     }
 
-
-    @Ignore
     @Test
     public void testLowercaseIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("isogram"));
     }
 
-
-    @Ignore
     @Test
     public void testNotIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertFalse(iso.isIsogram("eleven"));
     }
 
-
-    @Ignore
     @Test
     public void testMediumLongIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("subdermatoglyphic"));
     }
 
-
-    @Ignore
     @Test
     public void testCaseInsensitive() {
         IsogramChecker iso = new IsogramChecker();
         assertFalse(iso.isIsogram("Alphabet"));
     }
 
-
-    @Ignore
     @Test
     public void testIsogramWithHyphen() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("thumbscrew-japingly"));
     }
 
-
-    @Ignore
     @Test
     public void testIsogramWithDuplicatedHyphen() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("six-year-old"));
     }
 
-
-    @Ignore
     @Test
     public void testMadeUpNameThatIsAnIsogram() {
         IsogramChecker iso = new IsogramChecker();
         assertTrue(iso.isIsogram("Emily Jung Schwartzkopf"));
     }
 
-
-    @Ignore
     @Test
     public void testDuplicatedCharacterInTheMiddleIsNotIsogram() {
         IsogramChecker iso = new IsogramChecker();
